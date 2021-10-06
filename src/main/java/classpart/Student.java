1a7489014b4c38c4c1841b11795e29fb0e984b49
kimgudals <gudals-kim@github.com>
2021-10-06 13:57:25 +0900

    [update] Student 클래스 생성 및 작성 main 클래스에서 실행 확인

---
+++ src/main/java/classpart/Student.java
@@ -0,0 +1,11 @@
+package classpart;
+
+public class Student {
+    public int studentID;
+    public String studentname;
+    public String address;
+
+    public void showSutdentInfo(){
+        System.out.println(studentname+","+address);
+    }
+}
--- src/main/java/classpart/StudentTest.java
+++ src/main/java/classpart/StudentTest.java
@@ -1,4 +1,11 @@
 package classpart;
 
 public class StudentTest {
+    public static void main(String[] args) {
+        Student studentLee = new Student();
+        studentLee.studentname = "이순신";
+        studentLee.address = "서울";
+
+        studentLee.showSutdentInfo();
+    }
 }
