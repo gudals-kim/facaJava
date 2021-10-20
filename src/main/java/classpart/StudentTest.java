0b576520c96b25fc0f1129e071b281dcefcd06cd
kimgudals <gudals-kim@github.com>
2021-10-20 19:54:24 +0900

    [update] student sout

--- src/main/java/classpart/StudentTest.java
+++ src/main/java/classpart/StudentTest.java
@@ -1,5 +1,7 @@
 package classpart;
 
+import java.sql.SQLOutput;
+
 public class StudentTest {
     public static void main(String[] args) {
         Student studentLee = new Student();
@@ -7,5 +9,14 @@ public class StudentTest {
         studentLee.address = "서울";
 
         studentLee.showStudentInfo();
+
+        Student studentkim = new Student();
+        studentkim.studentname = "김유신";
+        studentkim.address = "경주";
+
+        studentkim.showStudentInfo();
+
+        System.out.println(studentLee);
+        System.out.println(studentkim);
     }
 }
